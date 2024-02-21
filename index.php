ff4ded569dc764123778ab8a88698ba77c495475
Loc Trinh <trhuloc@gmail.com>
2024-02-21 09:21:50 -0600

    index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Fuel Management System</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js" defer></script>
+    <script src="/js/validation.js" defer></script>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Welcome to Fuel Management System</h1>
+        <div style="display: inline-block;">
+            <a href="register.php" style="text-decoration: none;">
+                <button style="padding: 10px 20px; margin-right: 10px;">Register</button>
+            </a>
+            <a href="login.php" style="text-decoration: none;">
+                <button style="padding: 10px 20px;">Log In</button>
+            </a>
+        </div>
+
+    </div>
+</body>
+
+</html>
